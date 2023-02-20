4f8d313dc6e9c1abc204db347198c41647d3c835
Prabhu <84372886+Prabhu260902@users.noreply.github.com>
2023-02-20 22:36:38 +0530

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -12,64 +12,64 @@ public:
 
 class LinkedList{
 	Node* head;
-	public:
-		LinkedList(){
-			head=NULL;
+public:
+	LinkedList(){
+		head=NULL;
+	}
+
+	void insertEnd(int data){
+		Node* newNode = new Node(data);
+		if(head==NULL){
+			head=newNode;
+			return ;
 		}
+		Node* temp=head;
+		while(temp->next!=NULL) temp=temp->next;
+		temp->next=newNode;
+	}
 
-		void insertEnd(int data){
-			Node* newNode = new Node(data);
-			if(head==NULL){
-				head=newNode;
-				return ;
-			}
-			Node* temp=head;
-			while(temp->next!=NULL) temp=temp->next;
-			temp->next=newNode;
+	void insertFront(int data){
+		Node* temp = new Node(data);
+		temp->next=head;
+		head=temp;
+	}
+
+
+	void insertMiddle(int data){
+		Node* newNode = new Node(data);
+		Node* ptr=NULL,*slow=head,*fast=head;
+		while(fast!=NULL and fast->next!=NULL){
+			ptr=slow;
+			slow=slow->next;
+			fast=fast->next->next;
 		}
+		newNode->next=ptr->next;
+		ptr->next=newNode;
+	}
 
-		void insertFront(int data){
-			Node* temp = new Node(data);
-			temp->next=head;
-			head=temp;
+
+	void insertPosition(int data,int pos){
+		if(pos==1){
+			insertFront(data);
+			return ;
 		}
-
-
-		void insertMiddle(int data){
-			Node* newNode = new Node(data);
-			Node* ptr=NULL,*slow=head,*fast=head;
-			while(fast!=NULL and fast->next!=NULL){
-				ptr=slow;
-				slow=slow->next;
-				fast=fast->next->next;
-			}
-			newNode->next=ptr->next;
-			ptr->next=newNode;
+		Node* newNode = new Node(data);
+		Node* temp=head;
+		for(int i=1;i<pos-1;i++){
+			temp=temp->next;
 		}
+		newNode->next=temp->next;
+		temp->next=newNode;
+	}
 
-
-		void insertPosition(int data,int pos){
-			if(pos==1){
-				insertFront(data);
-				return ;
-			}
-			Node* newNode = new Node(data);
-			Node* temp=head;
-			for(int i=1;i<pos-1;i++){
-				temp=temp->next;
-			}
-			newNode->next=temp->next;
-			temp->next=newNode;
+	void print(){
+		Node* temp=head;
+		while(temp!=NULL){
+			cout<<temp->data<<" ";
+			temp=temp->next;
 		}
-
-		void print(){
-			Node* temp=head;
-			while(temp!=NULL){
-				cout<<temp->data<<" ";
-				temp=temp->next;
-			}
-			cout<<endl;
-		}
+		cout<<endl;
+	}
 
 };
 
